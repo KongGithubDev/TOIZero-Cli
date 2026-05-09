5deabefe37fcfb0685c28efc91130701bfcfae23
Watcharapong Namsaeng <113459098+KongGithubDev@users.noreply.github.com>
2026-05-09 21:46:49 +0700

    Delete solutions directory

--- solutions/A1-024/main.c
+++
@@ -1,9 +0,0 @@
-// C solution for A1-024
-#include <stdio.h>
-
-int main() {
-    // Read input
-    // Your code here
-    
-    return 0;
-}
--- solutions/A1-028/main.c
+++
@@ -1,9 +0,0 @@
-// C solution for A1-028
-#include <stdio.h>
-
-int main() {
-    // Read input
-    // Your code here
-    
-    return 0;
-}
